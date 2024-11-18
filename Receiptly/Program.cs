0ab5479f99603f9b53bedcfaa4bae54bdaabd5ae
Kenneth Jansson <kenneth.jansson@gmail.com>
2024-11-19 00:25:47 +0100

    Scoped IOCRService

--- Receiptly/Program.cs
+++ Receiptly/Program.cs
@@ -12,6 +12,7 @@ namespace Receiptly
             builder.RootComponents.Add<HeadOutlet>("head::after");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddScoped<I>
 
             await builder.Build().RunAsync();
         }
