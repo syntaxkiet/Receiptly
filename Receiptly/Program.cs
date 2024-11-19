c9d1542cac386bc71c33748ef6734ae54a57725a
Kenneth Jansson <kenneth.jansson@gmail.com>
2024-11-19 02:01:18 +0100

    Registered HTTP client for API calls

--- Receiptly/Program.cs
+++ Receiptly/Program.cs
@@ -15,7 +15,7 @@ namespace Receiptly
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
             builder.Services.AddScoped<IOCRService, TesseractService>();
-
+                    builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7018/") });
             await builder.Build().RunAsync();
         }
     }
