1cd9cc98e11fb5d1234f1713da6f6b7990c39f55
Mira Karjalainen <mira.karjalainen@consid.se>
2024-11-21 14:01:53 +0100

    Added models and imports

--- Receiptly/Models/Receipt.cs
+++ Receiptly/Models/Receipt.cs
@@ -2,5 +2,20 @@
 {
     public class Receipt
     {
+        public int Id { get; set; }
+
+        public string StoreName { get; set; }
+
+        public List<Item> Items { get; set; }
+    }
+ 
+
+    public class Item
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int Quantity { get; set; }
     }
 }
