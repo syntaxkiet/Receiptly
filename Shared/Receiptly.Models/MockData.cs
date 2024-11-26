4bc8df1f79bd109c166ef275800f17c521b63ccf
Mira Karjalainen <mira.karjalainen@consid.se>
2024-11-26 12:26:58 +0100

    Modify mockdata with additional receipt and bestbeforedate

--- Shared/Receiptly.Models/MockData.cs
+++ Shared/Receiptly.Models/MockData.cs
@@ -2,55 +2,31 @@ namespace Shared.Receiptly.Models;
 
 public class MockData
 {
-    public static List<Receipt> GetMockReceipt()
+    public static List<Receipt> receiptList { get; set; } = new List<Receipt>
     {
-        return new List<Receipt>
+        new Receipt
         {
-            new Receipt{
             Id = 1,
-            StoreName = "Hemköp Gallerian",
-            PurchaseDate = new DateTime(2024, 10, 25, 15, 47, 55),
+            StoreName = "Hemköp",
+            PurchaseDate = DateTime.Now.AddDays(-5),
             Items = new List<Item>
             {
-                new Item
-                {
-                    Id = 1,
-                    Name = "MJÖLK 3% 1L",
-                    Quantity = 1,
-                    
-                },
-                new Item
-                {
-                    Id = 2,
-                    Name = "Bröd",
-                    Quantity = 1,
-                    
-                },
-                new Item
-                {
-                    Id = 3,
-                    Name = "Ost",
-                    Quantity = 1,
-                  
-                },
-                new Item
-                {
-                    Id = 4,
-                    Name = "JULMUST SOCKERFRI 1.4L",
-                    Quantity = 1,
-                    
-                },
-                new Item
-                {
-                    Id = 5,
-                    Name = "Ägg",
-                    Quantity = 12,
-                    
-                }
-                }
+                new Item { Id = 1, Name = "Mjölk", Quantity = 1, BestBeforeDate = null },
+                new Item { Id = 2, Name = "Bröd", Quantity = 2, BestBeforeDate = DateTime.Now.AddDays(5) },
+                new Item { Id = 3, Name = "Ägg", Quantity = 1, BestBeforeDate = DateTime.Now.AddDays(10) }
             }
-
-        };
-            
-    }
+        },
+        new Receipt
+        {
+            Id = 2,
+            StoreName = "ICA",
+            PurchaseDate = DateTime.Now.AddDays(-3),
+            Items = new List<Item>
+            {
+                new Item { Id = 4, Name = "Smör", Quantity = 1, BestBeforeDate = null },
+                new Item { Id = 5, Name = "Ost", Quantity = 1, BestBeforeDate = DateTime.Now.AddDays(7) }
+            }
+        }
+    };
+    
 }
