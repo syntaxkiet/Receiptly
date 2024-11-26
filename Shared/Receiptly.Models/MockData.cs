e22cae1fd20f5fa97cfe4aac2d2e1b01329619d1
Mira Karjalainen <mira.karjalainen@consid.se>
2024-11-26 09:10:34 +0100

    Added content to models

--- Shared/Receiptly.Models/MockData.cs
+++ Shared/Receiptly.Models/MockData.cs
@@ -2,5 +2,52 @@ namespace Shared.Receiptly.Models;
 
 public class MockData
 {
-    
+    public static Receipt GetMockReceipt()
+    {
+        return new Receipt
+        {
+            Id = 1,
+            StoreName = "Hemköp Gallerian",
+            PurchaseDate = new DateTime(2024, 10, 25, 15, 47, 55),
+            Items = new List<Item>
+            {
+                new Item
+                {
+                    Id = 1,
+                    Name = "MJÖLK 3% 1L",
+                    Quantity = 1,
+                    
+                },
+                new Item
+                {
+                    Id = 2,
+                    Name = "Bröd",
+                    Quantity = 1,
+                    
+                },
+                new Item
+                {
+                    Id = 3,
+                    Name = "Ost",
+                    Quantity = 1,
+                  
+                },
+                new Item
+                {
+                    Id = 4,
+                    Name = "JULMUST SOCKERFRI 1.4L",
+                    Quantity = 1,
+                    
+                },
+                new Item
+                {
+                    Id = 5,
+                    Name = "Ägg",
+                    Quantity = 12,
+                    
+                }
+            }
+
+        };
+    }
 }
--- Shared/Receiptly.Models/Receipt.cs
+++ Shared/Receiptly.Models/Receipt.cs
@@ -2,5 +2,13 @@ namespace Shared.Receiptly.Models;
 
 public class Receipt
 {
+    public int Id { get; set; }
+    
+    public string StoreName { get; set; }
+    
+    public DateTime PurchaseDate { get; set; }  
+    
+    public List<Item> Items { get; set; }
+    
     
 }
