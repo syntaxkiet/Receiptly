6c52bf71fb40a93d0896a9aa6ec13a057078ac48
Kenneth Jansson <kenneth.jansson@gmail.com>
2024-11-29 14:16:30 +0100

    Add IronOcr NuGet package and remove ambiguos libraries in TesseractService

--- Shared/Service/Tesseract/TesseractService.cs
+++ Shared/Service/Tesseract/TesseractService.cs
@@ -4,8 +4,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Drawing;
-using System.Drawing.Imaging;
 using Shared.Interface;
 using Tesseract;
 using SixLabors.ImageSharp;
