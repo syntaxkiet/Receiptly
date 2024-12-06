317a13cc400f4ec80802d8a524f949aa868a0ada
Kenneth Jansson <kenneth.jansson@gmail.com>
2024-12-06 11:59:01 +0100

    Updated vulnerable NuGet packages in solution
    Might be packages installed in projects that don't rely on them!

---
+++ Shared/Service/ReceiptParser/ReceiptParser.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Shared.Service.ReceiptParser
+{
+    public class ReceiptParser
+    {
+
+        public static List<ReceiptItem> ExtractItems(string receipt)
+        {
+            var items = new List<ReceiptItem>();
+            string pattern = @"(?<Name>[A-ZÅÄÖa-zåäö\s]+?)\s(?<Quantity>\d*[,\.]?\d+)?\s?[A-Za-z]*\s?(?<UnitPrice>\d+[,\.]?\d+)?\s?(?<TotalPrice>\d+[,\.]?\d+)";
+            var matches = Regex.Matches(receipt, pattern);
+
+            foreach (Match match in matches)
+            {
+                var item = new ReceiptItem
+                {
+                    Name = match.Groups["Name"].Value.Trim(),
+                    Quantity = decimal.TryParse(match.Groups["Quantity"].Value, out var quantity) ? quantity : (decimal?)null,
+                    UnitPrice = decimal.TryParse(match.Groups["UnitPrice"].Value, out var unitPrice) ? unitPrice : (decimal?)null,
+                    TotalPrice = decimal.TryParse(match.Groups["TotalPrice"].Value, out var totalPrice) ? totalPrice : 0
+                };
+
+                items.Add(item);
+            }
+
+            return items;
+        }
+        public class ReceiptItem
+        {
+            public string Name { get; set; }
+            public decimal? Quantity { get; set; }
+            public decimal? UnitPrice { get; set; }
+            public decimal TotalPrice { get; set; }
+        }
+
+    }
+}
