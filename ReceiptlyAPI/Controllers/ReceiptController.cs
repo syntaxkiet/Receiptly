b72780190e34485d8e506a2b5dd0884e6b667b47
Anton Bubicic <anton.bubicic@gmail.com>
2024-12-08 17:14:05 +0100

    WPI Crud

---
+++ ReceiptlyAPI/Controllers/ReceiptController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ReceiptlyAPI.Services;
+using Shared.Models;
+
+namespace ReceiptlyAPI.Controllers;
+
+
+[Route("api/[controller]")]
+[ApiController]
+public class ReceiptController : ControllerBase
+{
+    private readonly ReceiptService _receiptService;
+
+    public ReceiptController(ReceiptService receiptService)
+    {
+        _receiptService = receiptService;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Receipt>>> GetReceipts()
+    {
+        var receipts = await _receiptService.GetReceiptsAsync();
+        return Ok(receipts);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult> CreateOrUpdateReceipt(List<Receipt> receipts)
+    {
+        await _receiptService.AddOrUpdateReceiptAsync(receipts);
+        return Ok(new { message = "Receupts have been created/updated successfully." });
+    }
+
+    [HttpDelete]
+    public async Task<IActionResult> DeleteReceipt(int id)
+    {
+        await _receiptService.DeleteReceiptAsync(id);
+        return NoContent();
+    }
+}
---
+++ ReceiptlyAPI/Services/ReceiptService.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ReceiptlyAPI.Data;
+using Shared.Models;
+
+namespace ReceiptlyAPI.Services;
+
+public class ReceiptService
+{
+    private readonly ReceiptlyDbContext _context;
+
+    public ReceiptService(ReceiptlyDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<List<Receipt>> GetReceiptsAsync()
+    {
+        return await _context.Receipts.ToListAsync();
+    }
+
+    public async Task<Receipt> GetReceiptByIdAsync(int id)
+    {
+        return await _context.Receipts.FindAsync(id);
+    }
+
+    public async Task AddOrUpdateReceiptAsync(List<Receipt> receipts)
+    {
+        foreach (var receipt in receipts)
+        {
+            var existingReceipt = await _context.Receipts.FindAsync(receipt.Id);
+            if(existingReceipt == null)
+            {
+                _context.Receipts.Add(receipt);
+            }
+            else
+            {
+                existingReceipt.PurchaseDate = receipt.PurchaseDate;
+                existingReceipt.StoreName = receipt.StoreName;
+                existingReceipt.Items = receipt.Items;
+                _context.Receipts.Update(existingReceipt);
+            }
+        }
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task DeleteReceiptAsync(int id)
+    {
+        var receipt = await _context.Receipts.FindAsync(id);
+        if (receipt == null)
+        {
+            _context.Receipts.Remove(receipt);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
